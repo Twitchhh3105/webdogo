adc7d9dbfbc3efeb99ebbb817fd3ee8863cc1ca9
Thach-123-aa <haikyuu2233@gmail.com>
2026-02-24 22:05:46 +0700

    fix2

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
-import { visionTool } from '@sanity/vision'
+import { structureTool } from 'sanity/structure'
 import { schemaTypes } from './src/sanity/schemaTypes'
 
 export default defineConfig({
@@ -12,7 +11,7 @@ export default defineConfig({
 
   basePath: '/admin',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
